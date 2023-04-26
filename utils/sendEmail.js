81a1460e4ded672fdc6dcf19be9ca460414eaaae
emmamarks <37787980+emmamarks@users.noreply.github.com>
2023-04-26 17:26:23 +0100

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = (options) =>{
     })
 
     const mailOptions = {
-        from: process.env.EMAIL_FROM,
+        from: "decgds<onwardfrancis@yahoo.com>",
         to: options.to,
         subject: options.subject,
         html: options.text
