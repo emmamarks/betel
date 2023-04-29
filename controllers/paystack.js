bb82c2d21d1fc205b09971e03974b29fcc8cf19c
Ephraim <aigbefoephraim@yahoo.com>
2023-04-29 11:58:53 +0100

    fetch banks and resolve account numbers

---
+++ controllers/paystack.js
@@ -0,0 +1,27 @@
+const { default: axios } = require("axios");
+
+exports.listBanks = async (req, res, next) => {
+  try {
+    const result = await axios.get(`${process.env.PAYSTACK_API}/bank`, {
+      headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}` },
+    });
+    return res.status(200).json(result.data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.resolveAccountNumber = async (req, res, next) => {
+  try {
+    const { accountNumber, bankCode } = req.body;
+    const result = await axios.get(
+      `${process.env.PAYSTACK_API}/bank/resolve?account_number=${accountNumber}&bank_code=${bankCode}`,
+      {
+        headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}` },
+      }
+    );
+    return res.status(200).json(result.data);
+  } catch (error) {
+    next(error);
+  }
+};
